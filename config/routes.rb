d68216d439c8a95e64c7ab962cb7ed12c18dee9c
Wineshuga <uzochukwuwinnie@gmail.com>
2023-11-20 22:40:47 +0100

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   
   root "home#index"
+  resources :categories, only: [:index, :show, :new, :create] do
+    resources :procurements, only: [:index, :show, :new, :create]
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
 
