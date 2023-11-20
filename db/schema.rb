5997ea72f5e6f3fa8fb1db902b7940235d886d59
Wineshuga <uzochukwuwinnie@gmail.com>
2023-11-20 13:31:21 +0100

    Add user reference to the categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_20_115334) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_20_122731) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_20_115334) do
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_categories_on_author_id"
   end
 
   create_table "categories_procurements", id: false, force: :cascade do |t|
@@ -41,5 +43,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_20_115334) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "categories", "users", column: "author_id"
   add_foreign_key "procurements", "users", column: "author_id"
 end
