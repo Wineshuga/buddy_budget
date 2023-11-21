7abe2d4f0e8882e8079024ad88fcab3a6853aa0c
Wineshuga <uzochukwuwinnie@gmail.com>
2023-11-21 15:11:58 +0100

    Create procurements_controller new, index actions

--- app/controllers/procurements_controller.rb
+++ app/controllers/procurements_controller.rb
@@ -1,3 +1,12 @@
 class ProcurementsController < ApplicationController
   before_action :authenticate_user!
+  
+  def new
+    @procurement = Procurement.new
+  end
+
+  def index
+    @category = Category.find(params[:category_id])
+    @procurements = @category.procurements.order(created_at: :desc)
+  end
 end
