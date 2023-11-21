7ad2bca117cde64568a421e43f67899c3881e09b
Wineshuga <uzochukwuwinnie@gmail.com>
2023-11-21 16:13:58 +0100

    Add category new page and action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,4 +8,23 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.where(author_id: current_user.id)
   end
+  
+  def create
+    @category = Category.new(category_params)
+    @category.author_id = current_user.id
+
+    if @category.save
+      flash[:notice] = 'A Category has been created!'
+      redirect_to categories_path
+    else
+      flash[:alert] = "Error! #{@category.errors.full_messages.join(', ')}"
+      redirect_to new_category_path
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
 end
