144ab2e13fd7e5b96d99a1941977bba5b19e4ef0
Wineshuga <uzochukwuwinnie@gmail.com>
2023-11-21 15:10:53 +0100

    Create categories_controller new and index actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,11 @@
 class CategoriesController < ApplicationController
   before_action :authenticate_user!
+    
+  def new
+    @category = Category.new
+  end
+
+  def index
+    @categories = Category.where(author_id: current_user.id)
+  end
 end
