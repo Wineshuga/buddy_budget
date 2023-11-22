55edad4d98caf41eded0ed03d28dbf8298af9cd4
Wineshuga <uzochukwuwinnie@gmail.com>
2023-11-22 12:18:55 +0100

    Add create action to procurement controller

--- app/controllers/procurements_controller.rb
+++ app/controllers/procurements_controller.rb
@@ -3,10 +3,30 @@ class ProcurementsController < ApplicationController
   
   def new
     @procurement = Procurement.new
+    @categories = Category.where(author_id: current_user.id)
   end
 
   def index
     @category = Category.find(params[:category_id])
     @procurements = @category.procurements.order(created_at: :desc)
+  end
+
+  def create
+    @procurement = Procurement.new(procurement_params)
+    @procurement.author_id = current_user.id
+
+    if @procurement.save
+      flash[:notice] = 'A transaction has been created!'
+      redirect_to category_procurements_path
+    else
+      flash[:alert] = "Error! #{@procurement.errors.full_messages.join(', ')}"
+      redirect_to new_category_procurement_path
+    end
+  end
+
+  private
+
+  def procurement_params
+    params.require(:procurement).permit(:name, :amount, category_ids: [])
   end
 end
