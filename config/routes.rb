3d44e64bf5a33c60c138531c9c6fe92fb885bfdf
Wineshuga <uzochukwuwinnie@gmail.com>
2023-11-22 13:18:07 +0100

    Customize routes to procurement and new pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  root "home#index"
-  resources :categories, only: [:index, :new, :create] do
-    resources :procurements, only: [:index, :show, :new, :create]
+  root "categories#index"
+  resources :categories, only: [ :create] do
+    collection do
+      get 'add_a_category', to: 'categories#new', as: 'new'
+    end
+    resources :procurements, only: [:index, :show, :create], path: 'transactions' do
+      collection do
+        get 'add_a_transaction', to: 'procurements#new', as: 'new'
+      end
+    end
   end
 
   get "up" => "rails/health#show", as: :rails_health_check
