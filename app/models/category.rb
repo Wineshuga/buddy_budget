e44e78b84636c36fcfda3ab90cadc39fe60cd30a
Wineshuga <uzochukwuwinnie@gmail.com>
2024-02-08 22:12:48 +0100

    Modify category model it accept an attachment

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,7 @@ class Category < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :categories_procurements, class_name: 'CategoriesProcurement'
   has_many :procurements, through: :categories_procurements
+  has_one_attached :image
 
   validates :name, presence: true
 end
